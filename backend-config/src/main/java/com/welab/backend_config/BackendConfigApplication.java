b223c3e72a6479068aa794ab668f1641553e643b
damiiya <ddam2lee@gmail.com>
2025-06-10 22:45:38 +0900

    lecture. backend-config 추가

---
+++ backend-config/src/main/java/com/welab/backend_config/BackendConfigApplication.java
@@ -0,0 +1,13 @@
+package com.welab.backend_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendConfigApplication.class, args);
+	}
+
+}
